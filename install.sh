8e4d9a64cdcf7c721615f8419ddb8f45a57d1a54
root@Optiplex-790 <liyalinux@protonmail.com>
2023-06-20 16:49:13 +0000

    add autoinstall

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "### Installing Kavya ###"
+echo "### Downloading Kavya ###"
 git clone https://gitlab.com/liya5/kavya.git /opt/repo/kavya
 echo "### Checking Chaotic-Aur ###"
 pacman -Q chaotic-keyring && pacman -Q chaotic-mirrorlist
@@ -7,16 +7,22 @@ if [[ $? -eq 1 ]]; then
 echo "### Chaotic-AUR is not installed, installing right away ###"
 cd /tmp && wget https://hyd1-in-mirror.silky.network/chaotic-aur/chaotic-aur/x86_64/chaotic-keyring-20230616-1-any.pkg.tar.zst && wget https://hyd1-in-mirror.silky.network/chaotic-aur/chaotic-aur/x86_64/chaotic-mirrorlist-20230603-1-any.pkg.tar.zst
 pacman -U chaotic-mirrorlist-20230603-1-any.pkg.tar.zst chaotic-keyring-20230616-1-any.pkg.tar.zst --noconfirm
-fi
 echo "### Backing Up Pacman.conf ###"
 cp /etc/pacman.conf /etc/pacman.conf.bak.1
+echo " " >> /etc/pacman.conf
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf 
+fi
+if [[ $? -eq 0 ]]; then
+echo "### Chaotic-AUR is instalelled. ###"
+fi
+echo "### Installing Kavya ###"
 echo " " >> /etc/pacman.conf
 echo "[kavya]" >> /etc/pacman.conf
 echo "SigLevel = Never" >> /etc/pacman.conf
 echo "Server = file:///opt/repo/kavya" >> /etc/pacman.conf
-echo " " >> /etc/pacman.conf
-echo "[chaotic-aur]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf 
+echo "Server = https://gitlab.com/liya5/kavya/-/raw/main/" >> /etc/pacman.conf
+echo "### Copying LISO ###"
 cp -rf Scripts/* /usr/bin
 mkdir -p /usr/share/liso
 chmod +x /usr/bin/mkiso
