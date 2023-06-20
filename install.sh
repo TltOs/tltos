c2ed58d1ea8f1d12f8c9f0645d3a8c7fc610be52
root@Optiplex-790 <liyalinux@protonmail.com>
2023-06-20 16:24:50 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo cp -rf Scripts/* /usr/bin
+mkdir -p /usr/share/liso
 chmod +x /usr/bin/mkiso
 sudo cp -rf ISO-Profiles /usr/share/liso/iso-profiles
 sudo cp -rf Components /usr/share/liso/components
