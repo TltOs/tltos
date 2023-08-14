b54494857958bc3f7f9552722665e4d72765efb9
SAYED M. Hisham <drhishamsayed128@protonmail.com>
2023-08-14 08:12:56 +0530

    Updatye

--- ISO-Profiles/common/profiledef.sh
+++ ISO-Profiles/common/profiledef.sh
@@ -20,4 +20,6 @@ file_permissions=(
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/local/bin/liya-update"]="0:0:755"
+  ["/usr/local/bin/linver"]="0:0:755"
+  ["/usr/local/bin/tpmcheck"]="0:0:755"
 )
